d5e948064f54806b50c944c85dd39856e24b64a7
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-10-15 21:56:25 +0600

    player name component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div className="" id="game-container">
+        <ol id="players">
+           <Player initialName="player one" symbol="X" />
+           <Player initialName="player two" symbol="O"/>
+            
+          
+        </ol>
+        Game Board
+      </div>
+      Log
+    </main>
+  );
 }
 
-export default App
+export default App;
