552bc9987c04d5cb00d551360a6f80bd2f8c4b63
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-10-16 21:30:39 +0600

    active  player logic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import {useState} from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSquareSelect() {
+    setActivePlayer((currentPlayer) => currentPlayer === "X" ? "O" : "X");
+  }
   return (
     <main>
       <div className="" id="game-container">
-        <ol id="players">
-           <Player initialName="player one" symbol="X" />
-           <Player initialName="player two" symbol="O"/>
+        <ol id="players" className="highlight-player">
+           <Player initialName="player one" symbol="X" isActive={activePlayer === "X"}/>
+           <Player initialName="player two" symbol="O" isActive={activePlayer === "O"}/>
             
           
         </ol>
-        Game Board
+        <GameBoard onSelectSquare={handleSquareSelect} activePlayerSymbol={activePlayer}/>
       </div>
       Log
     </main>
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSquareClick(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedGameBoard;
+    });
+    onSelectSquare();
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSquareClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
